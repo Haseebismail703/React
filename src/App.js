b279c238910fa015c0abf1cfed383c76bb862a97
haseebismail703 <haseebismail703@gmail.com>
2024-02-11 21:18:36 +0500

    blog

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Router from './Config/Router'
+
+function App() {
+  return (
+    <div >
+      <Router />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Note.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Note() {
+  return (
+    <div>
+        <h1>Page Note found</h1>
+    </div>
+  )
+}
+
+export default Note
